14ae6ba10fd1f6ceb5ce624d096f4f734e076408
nbmnb69 <ngiabao2004@gmail.com>
2020-01-25 15:46:23 +0700

    sửa lỗi 1 tí

--- Vision/src/main/java/frc/robot/Robot.java
+++ Vision/src/main/java/frc/robot/Robot.java
@@ -93,67 +93,60 @@ public class Robot extends TimedRobot{
         // Tìm cái phản quang
         vertices = findTarget(inputContours);
 
-        {
-          all = vertices.toArray();
-          for(int o = 0; o < 8; o++){
-            coords[o][0] = all[o].x;
-            coords[o][1] = all[o].y;
-          }
-          Arrays.sort(coords, (b, a) -> Double.compare(b[0], a[0]));
-          for(int o = 0; o < 8; o++){
-            if(o < 4){
-              top4[o][0] = coords[o][0];
-              top4[o][1] = coords[o][1];
-            } else if(o < 6){
-              bot1[o][0] = coords[o][0];
-              bot1[o][1] = coords[o][1];
-            } else{
-              bot2[o][0] = coords[o][0];
-              bot2[o][1] = coords[o][1];
-            }
-          }
-          Arrays.sort(top4, (a, b) -> Double.compare(a[0], b[0]));
-          a1 = top4[0][1] - top4[0][0] / top4[1][1] - top4[1][0];
-          b1 = top4[0][1] - a1*top4[0][0];
-          a2 = -1/a1;
-          b2 = top4[2][1] - a2*top4[2][0];
-          top4[2][0] = (b2-b1)/(a1-a2);
-          top4[2][1] = a1*top4[2][0] + b1;
-          a3 = -1/a1;
-          b3 = top4[3][1] - a3*top4[3][0];
-          top4[3][0] = (b3-b1)/(a1-a3);
-          top4[3][1] = a3*top4[3][0] + b3;
-          for(int o = 0; o < 4; o++){
-            processed[o] = new Point(top4[o][0], top4[o][1]);
-          }
-          bb1 = bot1[0][1] - a1*bot1[0][0];
-          a4 = -1/a1;
-          b4 = bot1[1][1] - a4*bot1[1][0];
-          bot1[1][0] = (b4-bb1)/(a1-a4);
-          bot1[1][1] = a4*bot1[1][0] + b4;
-          processed[4] = new Point(bot1[0][0], bot1[0][1]);
-          processed[5] = new Point(bot1[1][0], bot1[1][1]);
-          bb2 = bot2[0][1] - a1*bot2[0][0];
-          a5 = -1/a1;
-          b5 = bot2[1][1] - a5*bot2[1][0];
-          bot2[1][0] = (b5-bb2)/(a1-a5);
-          bot2[1][1] = a5*bot2[1][0] + b5;
-          processed[7] = new Point(bot2[0][0], bot2[0][1]);
-          processed[8] = new Point(bot2[1][0], bot2[1][1]);
-          //.
-          //continue here continue here continue here continue here continue here continue here continue here continue here continue here
-          //.
+
+        all = vertices.toArray();
+        for(int o = 0; o < 8; o++){
+          coords[o][0] = all[o].x;
+          coords[o][1] = all[o].y;
+        }
+        Arrays.sort(coords, (a, b) -> Double.compare(b[0], a[0]));
+        for(int o = 0; o < 8; o++){
+          if(o < 4){
+            top4[o][0] = coords[o][0];
+            top4[o][1] = coords[o][1];
+          } else if(o < 6){
+            bot1[o-4][0] = coords[o][0];
+            bot1[o-4][1] = coords[o][1];
+          } else{
+            bot2[o-6][0] = coords[o][0];
+            bot2[o-6][1] = coords[o][1];
           }
         }
+        Arrays.sort(top4, (a, b) -> Double.compare(a[0], b[0]));
+        a1 = top4[0][1] - top4[0][0] / top4[1][1] - top4[1][0];
+        b1 = top4[0][1] - a1*top4[0][0];
+        a2 = -1/a1;
+        b2 = top4[2][1] - a2*top4[2][0];
+        top4[2][0] = (b2-b1)/(a1-a2);
+        top4[2][1] = a1*top4[2][0] + b1;
+        a3 = -1/a1;
+        b3 = top4[3][1] - a3*top4[3][0];
+        top4[3][0] = (b3-b1)/(a1-a3);
+        top4[3][1] = a3*top4[3][0] + b3;
+        for(int o = 0; o < 4; o++){
+          processed[o] = new Point(top4[o][0], top4[o][1]);
+        }
+        bb1 = bot1[0][1] - a1*bot1[0][0];
+        a4 = -1/a1;
+        b4 = bot1[1][1] - a4*bot1[1][0];
+        bot1[1][0] = (b4-bb1)/(a1-a4);
+        bot1[1][1] = a4*bot1[1][0] + b4;
+        processed[4] = new Point(bot1[0][0], bot1[0][1]);
+        processed[5] = new Point(bot1[1][0], bot1[1][1]);
+        bb2 = bot2[0][1] - a1*bot2[0][0];
+        a5 = -1/a1;
+        b5 = bot2[1][1] - a5*bot2[1][0];
+        bot2[1][0] = (b5-bb2)/(a1-a5);
+        bot2[1][1] = a5*bot2[1][0] + b5;
+        processed[7] = new Point(bot2[0][0], bot2[0][1]);
+        processed[8] = new Point(bot2[1][0], bot2[1][1]);
+        //.
+        //continue here continue here continue here continue here continue here continue here continue here continue here continue here
+        //.
+        }
         
-        // !!!
-        // CHỌN CHỈ 1 TRONG 2 CÁI DƯỚI
-        // !!!
-
-        // 1) lệnh xử lý toàn bộ viền một contour
-        // Imgproc.drawContours(frame, outputContours, -69420, new Scalar(255, 255, 255));
         
-        // 2) nối các đỉnh/góc của contour
+        // Nối 8 đỉnh đã xử lí
         MatOfPoint passoagresso = new MatOfPoint(processed);
         List<MatOfPoint> fuckyou = new ArrayList<MatOfPoint>();
         fuckyou.add(passoagresso);
